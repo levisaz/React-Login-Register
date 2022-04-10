a79aa7f1b05d751402e58db47c23b2ea487fd8be
Levi <johnleviticus.sazon@gmail.com>
2022-04-10 15:18:29 +0800

    added register initial form

--- 4ITG_Act3_SazonValenzuela/src/components/Register.js
+++ 4ITG_Act3_SazonValenzuela/src/components/Register.js
@@ -1,9 +1,66 @@
-import React from 'react'
-
+import React from "react";
+import "../styles/Register.scss";
+import { Container, Form, Button } from "react-bootstrap";
 const Register = () => {
   return (
-    <div>Register</div>
-  )
-}
+    <Container className="register mt-5 d-flex justify-content-center">
+    
+        <Form>
+        <h2>Register</h2>
+            <Form.Group className="mb-3 form-group" controlId="formBasicEmail">
+                <Form.Label>Student ID Number</Form.Label>
+                <Form.Control type="email" placeholder="2018123456" />
+            </Form.Group>
 
-export default Register
+            <Form.Group className="mb-3 form-group" controlId="formBasicEmail">
+                <Form.Label>Last Name</Form.Label>
+                <Form.Control type="email" placeholder="Doe" />
+            </Form.Group>
+
+            <Form.Group className="mb-3 form-group" controlId="formBasicEmail">
+                <Form.Label>Given Name</Form.Label>
+                <Form.Control type="email" placeholder="John" />
+            </Form.Group>
+
+            <Form.Group className="mb-3 form-group" controlId="formBasicEmail">
+                <Form.Label>Middle Name</Form.Label>
+                <Form.Control type="email" placeholder="Chesterfieeld" />
+            </Form.Group>
+
+            <Form.Group className="mb-3 form-group" controlId="formBasicEmail">
+                <Form.Label>College</Form.Label>
+                <Form.Control type="email" placeholder="CICS" />
+            </Form.Group>
+
+            <Form.Group className="mb-3 form-group" controlId="formBasicEmail">
+                <Form.Label>Program Enrolled</Form.Label>
+                <Form.Control type="email" placeholder="BS Information Technology" />
+            </Form.Group>
+
+            <Form.Group className="mb-3 form-group" controlId="formBasicEmail">
+                <Form.Label>Year Level</Form.Label>
+                <Form.Control type="email" placeholder="4" />
+            </Form.Group>
+
+            <Form.Group className="mb-3 form-group" controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+
+            <Form.Group className="mb-3 form-group" controlId="formBasicPassword">
+                <Form.Label>Confirm Password</Form.Label>
+                <Form.Control type="password" placeholder="" />
+            </Form.Group>
+
+            <Form.Group className="mb-3 form-group" controlId="formBasicCheckbox">
+                <Form.Check type="checkbox" label="Check me out" />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+                Submit
+            </Button>
+        </Form>
+    </Container>
+  );
+};
+
+export default Register;
